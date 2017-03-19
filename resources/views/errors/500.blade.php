6cf9bb188e553070c3d294a4ba375a78fae9b504
ashton <luckyguy73@yahoo.com>
2017-03-19 16:21:54 -0700

    adding view for 500 server error

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.error')
+@section('content')
+	<div class="col-sm-8 col-sm-offset-2 vertical-center">
+		<div class="panel panel-default">
+			<div class="panel-heading text-center">
+				<h1>Whoops, something went wrong!</h1>
+				<div class="text-danger" style="font-size: 2.2rem">500 Internal Server Error</div>
+				<br>
+				<a href="{{ route('posts.index') }}"><button class="btn btn-danger">Go Back</button></a>
+			</div>
+			<div class="panel-body text-center">
+				<img src="/images/500.png">
+			</div>
+		</div>
+	</div>
+@stop
