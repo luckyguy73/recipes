441a21a481ba004ed8327f7eb011b76628da7d1f
ashton_morgan <luckyguy73@yahoo.com>
2019-10-23 01:42:39 -0700

    add data folder to storage session

--- resources/views/layouts/partials/flash.blade.php
+++ resources/views/layouts/partials/flash.blade.php
@@ -2,7 +2,6 @@
     <div class="alert alert-success alert-dismissable flash-message">
         <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         {{ session('success') }}
-        
     </div>
 @endif
 @if(session('danger'))
